c89ae72a5620e9d4e7463eb5ca73ab443c7dd64d
TrisTaam <yadartcon@gmail.com>
2023-08-17 00:23:47 +0700

    SharedViewModel

---
+++ app/src/main/java/dev/proptit/template/ui/password/PasswordFragment.kt
@@ -0,0 +1,43 @@
+package dev.proptit.template.ui.password
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import dev.proptit.template.R
+import dev.proptit.template.SharedViewModel
+import dev.proptit.template.databinding.FragmentPasswordBinding
+
+class PasswordFragment : Fragment() {
+    private var _binding: FragmentPasswordBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: SharedViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPasswordBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            viewModel.username.observe(viewLifecycleOwner) {
+                welcome.text = getString(R.string.welcome_back, it)
+            }
+            signInBtn.setOnClickListener {
+                Toast.makeText(requireContext(), "Sign In", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/dev/proptit/template/ui/username/UsernameFragment.kt
@@ -0,0 +1,41 @@
+package dev.proptit.template.ui.username
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import dev.proptit.template.R
+import dev.proptit.template.SharedViewModel
+import dev.proptit.template.databinding.FragmentUsernameBinding
+
+class UsernameFragment : Fragment() {
+    private var _binding: FragmentUsernameBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: SharedViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUsernameBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            continueBtn.setOnClickListener {
+                viewModel.setUsername(usernameEditText.text.toString())
+                findNavController().navigate(R.id.passwordFragment)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
